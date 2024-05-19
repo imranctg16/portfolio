848436d0ed45d52fb28d35b5ea160727a89d13c9
Mohammad Imran Hossain <imran.hossain@brainstaion23.com>
2024-05-20 00:42:26 +0600

    Hire Me Button Removed

--- src/components/shared/AppHeader.vue
+++ src/components/shared/AppHeader.vue
@@ -3,14 +3,12 @@ import ThemeSwitcher from '../ThemeSwitcher';
 import HireMeModal from '../HireMeModal.vue';
 import feather from 'feather-icons';
 import AppHeaderLinks from './AppHeaderLinks.vue';
-import Button from '../reusable/Button.vue';
 
 export default {
 	components: {
 		ThemeSwitcher,
 		HireMeModal,
 		AppHeaderLinks,
-		Button,
 	},
 	data() {
 		return {
@@ -143,15 +141,7 @@ export default {
 			<div
 				class="hidden sm:flex justify-between items-center flex-col md:flex-row"
 			>
-				<!-- Hire me button -->
-				<div class="hidden md:block">
-					<Button
-						title="Hire Me"
-						class="text-md font-general-medium bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-md px-5 py-2.5 duration-300"
-						@click="showModal()"
-						aria-label="Hire Me Button"
-					/>
-				</div>
+				
 
 				<!-- Theme switcher large screen -->
 				<theme-switcher
